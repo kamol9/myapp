938aeab2bccadcebd636f672b37f08a3a06245ca
kamol9 <maciek98@poczta.onet.pl>
2021-01-10 20:05:15 +0100

    .

--- src/helpers/api.js
+++
@@ -1,9 +0,0 @@
-export const get = url =>
-    new Promise(
-        (resolve, recejt) => {
-            fetch(url)
-                .then(respone => Response.json())
-                .then(jsno => resolve(json))
-        }
-    );
-
--- src/helpers/routes.js
+++
@@ -1,4 +0,0 @@
-const serverUrl = 'https://jsonplaceholder.typicode.com/posts/'
-
-export const toDoItemsApiUrl = id =>
-    id ? `${serverUrl}/todo_items/${id}` : `${serverUrl}/todo_items/`
